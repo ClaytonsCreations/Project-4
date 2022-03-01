50be26c8dca37a5cde353a2b3feb6fa5ee50ab49
ClaytonsCreations <clytn579@gmail.com>
2022-02-28 21:20:27 -0800

    heroku update

--- app.py
+++ app.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("templates/index.html")
 
 @app.route("/team")
 def team():
-    return render_template("team.html")
+    return render_template("templates/team.html")
 
 @app.route("/data")
 def data():
-    return render_template("data.html")
+    return render_template("templates/data.html")
 
 @app.route("/ML")
 def ML():
-    return render_template("machineLearning.html")
+    return render_template("templates/machineLearning.html")
 
 @app.route("/process")
 def process():
-    return render_template("process.html")
+    return render_template("templates/process.html")
 
 @app.route("/analysis")
 def analysis():
-    return render_template("analysis.html")
+    return render_template("templates/analysis.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
